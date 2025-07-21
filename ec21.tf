5c3a40e4ea05f2c614b5874cd8c84d821b8590c6
Queenivas <ombakuqueeniva@yahoo.co.uk>
2025-07-21 13:38:06 +0100

    new t

--- ec21.tf
+++ ec21.tf
@@ -2,10 +2,16 @@ provider "aws" {
   region = "us-west-1"  # Change to your preferred AWS region
 }
 
-# Optional: Create a key pair to SSH into the instance
+# Define a variable for the SSH public key
+variable "ssh_public_key" {
+  description = "Public key for SSH access"
+  type        = string
+}
+
+# Create a key pair to SSH into the instance
 resource "aws_key_pair" "my_key" {
   key_name   = "my-key"
-  public_key = file("~/.ssh/id_rsa.pub")  # Path to your public SSH key
+  public_key = var.ssh_public_key
 }
 
 # Security group allowing SSH access
@@ -31,13 +37,18 @@ resource "aws_security_group" "allow_ssh" {
 
 # EC2 instance
 resource "aws_instance" "example" {
-  count                       = "3"
-  ami                         = "ami-0c02fb55956c7d316"  # Amazon Linux 2 in us-east-1 (as of July 2025)
-  instance_type               = "t2.micro"
-  key_name                    = aws_key_pair.my_key.key_name
-  vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
+  count                  = 3
+  ami                    = "ami-0a0409af1cb831414"  # Amazon Linux 2 in us-west-1 (updated AMI)
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.my_key.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   tags = {
-    Name = "MyEC2Instance"
+    Name = "MyEC2Instance-${count.index + 1}"
   }
+}
+
+# Output the public IPs of the instances
+output "instance_public_ips" {
+  value = aws_instance.example[*].public_ip
 }
